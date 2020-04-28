ee7171b99ce5c33801b66d1a5e480fd7ac09bed2
Elodie B <elodie.bassibey@free.fr>
2020-04-28 10:12:29 +0200

    un peu de mise en forme (CSS et HTML)

--- templates/base.php
+++ templates/base.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <title><?= $title ?></title>
     <link href="<?= $style ?>.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&family=Lato&display=swap" rel="stylesheet">
 </head>
 
 <body>
--- templates/frontend/home.php
+++ templates/frontend/home.php
@@ -9,8 +9,8 @@ foreach ($episodes as $episode) {
     <div>
         <h2><a href="../Projet_4/index.php?route=episode&episodeId=<?= htmlspecialchars($episode->getId()); ?>"><?= htmlspecialchars($episode->getTitle()); ?></a></h2>
         <p><?= htmlspecialchars($episode->getContent()); ?></p>
-        <p><?= htmlspecialchars($episode->getCreatedAt()); ?></p>
-        <p><?= htmlspecialchars($episode->getLikes()); ?></p>
+        <p> Sorti le : <?= htmlspecialchars($episode->getCreatedAt()); ?></p>
+        <p>Likes : <?= htmlspecialchars($episode->getLikes()); ?></p>
     </div>
     <?php
 }
